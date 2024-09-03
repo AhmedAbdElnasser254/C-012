603a9b74a341918aaf8ac56f6d4c6d2cdd868432
start <start@DESKTOP-L1PH92H>
2024-09-03 20:06:58 +0300

    Add funccccc

--- C#012/Program.cs
+++ C#012/Program.cs
@@ -5,6 +5,40 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, World!");
+
+
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!"); 
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
